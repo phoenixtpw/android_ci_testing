7f323da00bd21449401a111c8190ae30eb111a39
Phoenix Tsai <phoenix@appscore.com.au>
2017-06-02 14:21:58 +1000

    +5

--- app/src/main/java/helloworld/app/com/myapplication/MainActivity.java
+++ app/src/main/java/helloworld/app/com/myapplication/MainActivity.java
@@ -5,6 +5,8 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.TextView;
 import android.widget.Toast;
+import com.crashlytics.android.Crashlytics;
+import io.fabric.sdk.android.Fabric;
 
 
 public class MainActivity extends AppCompatActivity {
@@ -12,6 +14,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Fabric.with(this, new Crashlytics());
         setContentView(R.layout.activity_main);
 
         final TextView num1View = (TextView) findViewById(R.id.num1);
